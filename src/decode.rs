7cfcb0a489034cca5f28a17f38b87ea042312da6
Jeremy T Hatcher <jeremyhatcher27@gmail.com>
2019-10-20 19:08:24 -0300

    improved decode table macro to use tt munching

--- src/decode.rs
+++ src/decode.rs
@@ -1,46 +1,109 @@
 #![allow(dead_code)]
 
-macro_rules! instr {
+macro_rules! instr2_16 {
     {
-        name: $instr_name:ident,
-        $(
-            encoding {
-                base: $base:expr,
-                operand: { $AA_name:ident; $AA_width:expr; $AA_shift:expr; $AA_default:expr },
-                operand: { $BB_name:ident; $BB_width:expr; $BB_shift:expr; $BB_default:expr },
-                operand: { $CC_name:ident; $CC_width:expr; $CC_shift:expr; $CC_default:expr }
-            }
-        ),*
+        @terminal $name:ident, $dset:ident, $base:expr,
+        $AA_name:ident, $AA_width:expr, $AA_shift:expr, $AA_default:expr,
+        $BB_name:ident, $BB_width:expr, $BB_shift:expr, $BB_default:expr,
+        $CC_name:ident, $CC_width:expr, $CC_shift:expr, $CC_default:expr
     } => {
-        {
-            let mut __set: Vec<(u16, InstrThumb16)> = Vec::new();
-            $(
-                let __it1 = ::std::cmp::max(1, 2u16.pow($AA_width));
-                let __it2 = ::std::cmp::max(1, 2u16.pow($BB_width));
-                let __it3 = ::std::cmp::max(1, 2u16.pow($CC_width));
-
-                for $AA_name in 0..__it1 {
-                    for $BB_name in 0..__it2 {
-                        for $CC_name in 0..__it3 {
-                            let hw = $base
-                                | ($AA_name << $AA_shift)
-                                | ($BB_name << $BB_shift)
-                                | ($CC_name << $CC_shift);
-                            __set.push((
-                                hw,
-                                InstrThumb16::$instr_name{
-                                    $AA_name: ($AA_default.unwrap_or($AA_name)) as u8,
-                                    $BB_name: ($BB_default.unwrap_or($BB_name)) as u8,
-                                    $CC_name: ($CC_default.unwrap_or($CC_name)) as u8
-                                }
-                            ));
+        let __it1 = ::std::cmp::max(1, 2u16.pow($AA_width));
+        let __it2 = ::std::cmp::max(1, 2u16.pow($BB_width));
+        let __it3 = ::std::cmp::max(1, 2u16.pow($CC_width));
+        for $AA_name in 0..__it1 {
+            for $BB_name in 0..__it2 {
+                for $CC_name in 0..__it3 {
+                    let hw = $base
+                        | ($AA_name << $AA_shift)
+                        | ($BB_name << $BB_shift)
+                        | ($CC_name << $CC_shift);
+                    $dset.push((
+                        hw,
+                        InstrThumb16::$name{
+                            $AA_name: ($AA_default.unwrap_or($AA_name)) as u8,
+                            $BB_name: ($BB_default.unwrap_or($BB_name)) as u8,
+                            $CC_name: ($CC_default.unwrap_or($CC_name)) as u8
                         }
-                    }
+                    ));
                 }
-            )*
-            __set
+            }
         }
     };
+}
+
+macro_rules! instr2 {
+    // First expansion
+    {
+        name: $name:ident,
+        $($tail:tt)*
+    } => {
+        {
+            let mut __decoded_set: Vec::<(u16, InstrThumb16)> = Vec::new();
+            instr2!{ @recurse $name, __decoded_set, $($tail)* }
+            __decoded_set
+        }
+    };
+
+    // Recursive 16-bit encoding expansion
+    {
+        @recurse
+        $name:ident,
+        $dset:ident,
+        encoding16: {
+            base: $base:expr,
+            operand: { $AA_name:ident; $AA_width:expr; $AA_shift:expr; $AA_default:expr },
+            operand: { $BB_name:ident; $BB_width:expr; $BB_shift:expr; $BB_default:expr },
+            operand: { $CC_name:ident; $CC_width:expr; $CC_shift:expr; $CC_default:expr }
+        },
+        $($tail:tt)*
+    } => {
+        instr2_16!{
+            @terminal $name, $dset, $base,
+            $AA_name, $AA_width, $AA_shift, $AA_default,
+            $BB_name, $BB_width, $BB_shift, $BB_default,
+            $CC_name, $CC_width, $CC_shift, $CC_default
+        }
+        instr2!{ @recurse $name, $dset, $($tail)* }
+    };
+
+    // Terminal 16-bit encoding expansion
+    {
+        @recurse
+        $name:ident,
+        $dset:ident,
+        encoding16: {
+            base: $base:expr,
+            operand: { $AA_name:ident; $AA_width:expr; $AA_shift:expr; $AA_default:expr },
+            operand: { $BB_name:ident; $BB_width:expr; $BB_shift:expr; $BB_default:expr },
+            operand: { $CC_name:ident; $CC_width:expr; $CC_shift:expr; $CC_default:expr }
+        }
+        $($tail:tt)*
+    } => {
+        instr2_16!{
+            @terminal $name, $dset, $base,
+            $AA_name, $AA_width, $AA_shift, $AA_default,
+            $BB_name, $BB_width, $BB_shift, $BB_default,
+            $CC_name, $CC_width, $CC_shift, $CC_default
+        }
+    };
+}
+
+fn test_instr2() -> Vec::<(u16, InstrThumb16)> {
+    instr2!{
+        name: ADDimm,
+        encoding16: {
+            base: 0x1C00,
+            operand: { rd; 3; 0; None },
+            operand: { rdn; 3; 3; None },
+            operand: { imm; 3; 6; None }
+        },
+        encoding16: {
+            base: 0x3000,
+            operand: { imm; 8; 0; None },
+            operand: { rdn; 3; 8; None },
+            operand: { rd; 0; 0; Some(0xFF) }
+        }
+    }
 }
 
 macro_rules! instructions {
@@ -55,7 +118,7 @@ macro_rules! instructions {
                 }
             )*
 
-            __set.sort_by(|a, b| a.0.cmp(&b.0) );
+            //__set.sort_by(|a, b| a.0.cmp(&b.0) );
             __set
         }
         
@@ -79,37 +142,37 @@ pub enum InstrThumb16 {
 
 fn generate_decode_table() -> Vec<(u16, InstrThumb16)> {
     instructions!{
-        instr!{
+        instr2!{
             name: ADDimm,
-            encoding {
+            encoding16: {
                 base: 0x1C00,
                 operand: { rd; 3; 0; None },
                 operand: { rdn; 3; 3; None },
                 operand: { imm; 3; 6; None }
             },
-            encoding {
+            encoding16: {
                 base: 0x3000,
                 operand: { imm; 8; 0; None },
                 operand: { rdn; 3; 8; None },
                 operand: { rd; 0; 0; Some(0xFF) }
             }
         },
-        instr!{
+
+        instr2!{
             name: ADDreg,
-            encoding {
-                base: 0x1800,
-                operand: { rm; 3; 6; None },
+            encoding16: {
+                base: 0x1C00,
+                operand: { rd; 3; 0; None },
                 operand: { rdn; 3; 3; None },
-                operand: { rd; 3; 0; None }
+                operand: { rm; 3; 6; None }
             },
-            encoding {
-                base: 0x4400,
-                operand: { rm; 4; 3; None },
+            encoding16: {
+                base: 0x3000,
                 operand: { rdn; 3; 0; None },
-                operand: { rd; 1; 7; None }
+                operand: { rm; 4; 3; None },
+                operand: { rd; 1; 7; Some(0xFF) }
             }
         }
-
     }
 }
 
